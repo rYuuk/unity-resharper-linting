5af35257c79877b4deb6994940432575affd54de
Robin Sharma <robin@wolf3d.io>
2022-07-12 15:34:55 +0200

    Add dotSettings and a sample script

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Test
+{
+    private const string constantStringWithWrongCase = "ok";
+
+    public string publicStringWithWrongStyle;
+
+    private string PrivateStringWithWrongStyle;
+
+    private string privateStringUnused;
+
+    public void Start()
+    {
+        Debug.Log(publicStringWithWrongStyle);
+        Debug.Log(PrivateStringWithWrongStyle);
+        Debug.Log(constantStringWithWrongCase);
+    }
+}
